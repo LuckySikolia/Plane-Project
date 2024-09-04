f50bf60ade668daf123f8df9301316d1360870b6
Lucky <103184937+LuckySikolia@users.noreply.github.com>
2024-09-05 00:57:52 +0300

    add propeller spin
    made the plane propeller spin along the z axis

--- Assets/Challenge 1/Scripts/PlanePropeller.cs
+++ Assets/Challenge 1/Scripts/PlanePropeller.cs
@@ -4,6 +4,8 @@ using UnityEngine;
 
 public class PlanePropeller : MonoBehaviour
 {
+    private float propellerRotationSpeed = 150.0f;
+
     // Start is called before the first frame update
     void Start()
     {
@@ -13,6 +15,7 @@ public class PlanePropeller : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        
+        //transform the propeller along the Z axis
+        transform.Rotate(Vector3.forward * Time.deltaTime * propellerRotationSpeed);
     }
 }
