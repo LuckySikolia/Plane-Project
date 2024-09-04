50e912a392f60f3334d719a5b1f10e57afd2e4a5
Lucky <103184937+LuckySikolia@users.noreply.github.com>
2024-09-04 18:01:35 +0300

    fix plane camera
    placed side plane view camera
    adjust speed and rotation speed
    add blank plane propeller script

--- Assets/Challenge 1/Scripts/FollowPlayerX.cs
+++ Assets/Challenge 1/Scripts/FollowPlayerX.cs
@@ -5,7 +5,10 @@ using UnityEngine;
 public class FollowPlayerX : MonoBehaviour
 {
     public GameObject plane;
-    private Vector3 offset;
+
+    
+    private Vector3 offset = new Vector3(14.4f, 1.9f, 0.2f);
+   
 
     // Start is called before the first frame update
     void Start()
@@ -14,8 +17,11 @@ public class FollowPlayerX : MonoBehaviour
     }
 
     // Update is called once per frame
-    void Update()
+    void LateUpdate()
     {
+        //apply camera position
         transform.position = plane.transform.position + offset;
+
+        
     }
 }
--- Assets/Challenge 1/Scripts/PlayerControllerX.cs
+++ Assets/Challenge 1/Scripts/PlayerControllerX.cs
@@ -5,7 +5,7 @@ using UnityEngine;
 public class PlayerControllerX : MonoBehaviour
 {
     public float speed = 10.0f;
-    public float rotationSpeed;
+    public float rotationSpeed = 150;
     public float verticalInput;
 
     // Start is called before the first frame update
